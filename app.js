fc6a8cc30f923d2d87501f49003270ab7ced3a67
mr.gaurav <gauravvr77@gmail.com>
2021-03-29 10:59:24 +0530

    V1.O

---
+++ app.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+require('@tensorflow/tfjs');
+const toxicity = require('@tensorflow-models/toxicity');
+
+const threshold = 0.9;
+
+let model;
+
+client.on('ready', async () => {
+  model = await toxicity.load(threshold);
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', async msg => {
+    if(msg.author.bot){
+      return;
+    }
+
+    let message = msg.content;
+    let predictions = await model.classify(message);
+
+    predictions.forEach(prediction => {
+      if(prediction.results[0].match){
+          let label = prediction.label;
+          msg.reply(`Warning!!! ${label} Detected, Please Delete the message.`);
+      }
+  });
+});
+
+client.login('ODI1NjAxMDE2ODg3NzcxMTQ2.YGAS3Q.7fH2GrHdlebtTerlaxS9yi5eYYI');
